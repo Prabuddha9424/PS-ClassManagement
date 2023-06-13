1f3229738fd526b8fc8e008de86f923f2ee1b609
Prabuddha Jayasekara <44451357+Prabuddha9424@users.noreply.github.com>
2023-06-13 23:01:03 +0530

    Login and use account creation completed.

--- src/com/PSDeveloper/PSClassManagement/controller/LoginFormController.java
+++ src/com/PSDeveloper/PSClassManagement/controller/LoginFormController.java
@@ -1,4 +1,61 @@
 package com.PSDeveloper.PSClassManagement.controller;
 
+import com.PSDeveloper.PSClassManagement.util.PasswordManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws RuntimeException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/PC_ClassManage",
+                    "root","1234");
+            String sql="SELECT * FROM user WHERE email=?";
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                if(PasswordManager.checkPassword(txtPassword.getText().trim(),resultSet.getString("password"))){
+                    System.out.println("completed");
+                }
+                else {
+                    new Alert(Alert.AlertType.WARNING,"Password wrong! Please try again").show();
+                }
+
+            }else {
+                new Alert(Alert.AlertType.WARNING,"User email not found!").show();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage());
+        }
+
+
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm","SignUp");
+    }
+
+    public void forgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+    private void setUi(String location,String title) throws IOException {
+        Stage stage=(Stage) context.getScene().getWindow();
+        stage.centerOnScreen();
+        stage.setTitle(title);
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+    }
 }
